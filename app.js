37f58ad38d739030d799c9ee6d30d646ed405a9b
akeefazzzmy <80236891+akeefazzzmy@users.noreply.github.com>
2023-12-30 21:07:14 +0800

    dynamic classes

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
             url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books:[
-                {title: 'Name of the wind', author: 'patrick rothfuss', img:'assets/a.jpg'},
-                {title: 'The way of kings', author: 'brandon sanderson', img:'assets/b.jpg'},
-                {title: 'The final empire', author: 'patrick rothfuss', img:'assets/c.jpg'}
+                {title: 'Name of the wind', author: 'patrick rothfuss', img:'assets/a.jpg', isFav:true},
+                {title: 'The way of kings', author: 'brandon sanderson', img:'assets/b.jpg', isFav:false},
+                {title: 'The final empire', author: 'patrick rothfuss', img:'assets/c.jpg', isFav:true}
             ]
         }
     },
