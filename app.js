5b92522a6d311ea31111403da97f5467b9f15a85
akeefazzzmy <80236891+akeefazzzmy@users.noreply.github.com>
2023-12-30 20:55:12 +0800

    attribute binding

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
     data(){
         return{
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
             books:[
-                {title: 'Name of the wind', author: 'patrick rothfuss'},
-                {title: 'The way of kings', author: 'brandon sanderson'},
-                {title: 'The final empire', author: 'patrick rothfuss'}
+                {title: 'Name of the wind', author: 'patrick rothfuss', img:'assets/a.jpg'},
+                {title: 'The way of kings', author: 'brandon sanderson', img:'assets/b.jpg'},
+                {title: 'The final empire', author: 'patrick rothfuss', img:'assets/c.jpg'}
             ]
         }
     },
